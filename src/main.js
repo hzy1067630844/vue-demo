ec6fffbb7cd0a53050604ffd1e2635ed614a1edf
RefrainH <1067630844@qq.com>
2018-09-29 09:19:35 +0800

    update test

--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 // The Vue build version to load with the `import` command
 // (runtime-only or standalone) has been set in webpack.base.conf with an alias.
 import Vue from 'vue'
+// import Vuex from 'vuex'
 import App from './App.vue'
 import router from './router/index.js'
 import VueRouter from 'vue-router'
@@ -9,8 +10,15 @@ import VueRouter from 'vue-router'
 Vue.config.productionTip = false;
 
 Vue.use(VueRouter);
+// Vue.use(Vuex);
 
-
+// var store = new Vuex.store({
+// 	state:{},
+// 	mutations:{},
+// 	getters:{
+		
+// 	}
+// })
 /* eslint-disable no-new */
 new Vue({
   el: '#app',
